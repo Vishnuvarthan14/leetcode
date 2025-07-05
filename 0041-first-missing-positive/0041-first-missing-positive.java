8facbb1767b04dd1a141d50099a99b1ecfb35b64
Vishnuvarthan14 <139092681+Vishnuvarthan14@users.noreply.github.com>
2025-07-05 15:34:26 +0530

    Time: 33 ms (6.09%), Space: 57.2 MB (58.18%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        
+        int n=nums.length;
+        Arrays.sort(nums);
+        LinkedHashSet <Integer> set = new LinkedHashSet<>();
+
+        for(int i:nums)
+            set.add(i);
+        int arr[] = new int[set.size()];
+            int l=0;
+
+            for(int i:set)
+                arr[l++]=i;
+
+        int i=0;
+        while(i<n&&arr[i]<=0)
+            i++;
+
+        System.out.println(i);
+        int k=1;
+        for(int j=i;j<arr.length;j++)
+        {
+            if(arr[j]!=k)
+                return k;
+            k++;
+        }
+
+        return k;
+    }
+}
