700a6e41c6a47c3b14c97bc5245f03283c7c330c
Vishnuvarthan14 <139092681+Vishnuvarthan14@users.noreply.github.com>
2025-08-10 22:29:45 +0530

    Time: 243 ms (10.08%), Space: 139.9 MB (72.58%) - LeetHub

---
+++ 3863-power-grid-maintenance/3863-power-grid-maintenance.java
@@ -0,0 +1,86 @@
+class Solution {
+   private ArrayList <ArrayList<Integer>> adjs = new ArrayList<>();
+   private HashMap <Integer,TreeSet<Integer>> map= new HashMap<>();
+
+    public int[] processQueries(int c, int[][] connections, int[][] queries) {
+        ArrayList <Integer> ans = new ArrayList<>();
+        int nodeMap[] = new int[c];
+        for(int i=0;i<c;i++)
+        {
+            adjs.add(new ArrayList<>());
+        }
+
+        for(int arr[]:connections)
+        {
+            adjs.get(arr[0]-1).add(arr[1]-1);
+            adjs.get(arr[1]-1).add(arr[0]-1);
+        }
+
+        boolean visited[] = new boolean[c];
+        int comp=1;
+        for(int i=0;i<c;i++)
+        {
+            if(!visited[i]){
+                map.put(comp,bfs(comp,i,visited,nodeMap));
+                comp++;
+                }
+        }
+           for(int arr[]:queries)
+           {
+            if(arr[0]==1)
+            {
+                int node=arr[1]-1;
+                int cmp=nodeMap[node];
+               if(map.get(cmp).isEmpty())
+                ans.add(-1);
+                else if(map.get(cmp).contains(node))
+                ans.add(node+1);
+                else
+                ans.add(map.get(cmp).first()+1);
+            }
+            else if(arr[0]==2)
+            {
+               int node=arr[1]-1;
+                int cmp=nodeMap[node];
+               if(map.get(cmp).contains(node))
+               {
+                map.get(cmp).remove(node);
+               }
+            }
+           }
+
+          int arr[] = new int[ans.size()];
+          for(int i=0;i<ans.size();i++)
+          {
+            arr[i]=ans.get(i);
+          }
+        return arr;
+    }
+
+
+    public TreeSet<Integer> bfs(int comp,int source,boolean visited[],int[] nodeMap)
+    {
+        Queue <Integer> q=new LinkedList<>();
+        TreeSet <Integer> set = new TreeSet<>();
+
+        q.add(source);
+        nodeMap[source]=comp;
+        visited[source]=true;
+
+        while(!q.isEmpty())
+        {
+            int val = q.poll();
+            visited[val]=true;
+            set.add(val);
+            for(int i:adjs.get(val))
+            {
+                if(!visited[i])
+                {
+                    q.add(i);
+                    nodeMap[i]=comp;
+                }
+            }
+        }
+        return set;
+    }
+}
