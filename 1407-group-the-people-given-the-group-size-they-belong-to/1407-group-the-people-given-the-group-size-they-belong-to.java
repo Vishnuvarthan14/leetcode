11ce6e93bdcf8675d30b4c32664dcea6fc1a8298
Vishnuvarthan14 <139092681+Vishnuvarthan14@users.noreply.github.com>
2025-09-03 19:37:33 +0530

    Time: 12 ms (5.39%), Space: 45.9 MB (6.79%) - LeetHub

---
+++ 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<List<Integer>> groupThePeople(int[] groupSizes) {
+        List<List<Integer>> list = new ArrayList<>();
+
+        HashMap <Integer,ArrayList<Integer>> map= new HashMap<>();
+        int n=groupSizes.length;
+        for(int i=0;i<n;i++)
+        {
+            int key=groupSizes[i];
+
+            if(!map.containsKey(key))
+                map.put(key,new ArrayList<Integer>());
+            map.get(key).add(i);
+        }
+
+        System.out.println(map);
+
+        for(int key:map.keySet())
+        {
+            // ArrayList temp = map.get(key);
+            // int len=temp.size();
+            List <Integer> new_list = new ArrayList<>();
+            int count=0;
+            for(int val:map.get(key))
+            {
+                count++;
+                new_list.add(val);
+                if(count%key==0)
+                {
+                    // System.out.println(count);
+                    list.add(new ArrayList<>(new_list));
+                    new_list.clear();
+                }
+            }
+        }
+        return list;
+    }
+}
