737415e20f4ebc243e9770fcce275d050e705718
Vishnuvarthan14 <139092681+Vishnuvarthan14@users.noreply.github.com>
2025-09-04 21:32:37 +0530

    Time: 16 ms (5.29%), Space: 57.3 MB (5.49%) - LeetHub

---
+++ 0890-lemonade-change/0890-lemonade-change.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        HashMap <Integer,Integer> map =new HashMap <>();
+        map.put(5,0);
+        map.put(10,0);
+        map.put(20,0);
+
+        for(int i:bills)
+        {
+           // System.out.println(map);
+            if(i==5)
+                map.put(i,map.get(i)+1);
+            else if(i==10)
+            {
+                if(map.get(5)==0)
+                    return false;
+                else{
+                    map.put(5,map.get(5)-1);
+                    map.put(10,map.get(10)+1);
+                    }
+            }
+            else
+            {
+                if(map.get(10)>=1&&map.get(5)>=1)
+                {
+                    map.put(5,map.get(5)-1);
+                    map.put(10,map.get(10)-1);
+                    map.put(20,map.get(20)+1);
+                }
+                else if(map.get(5)>=3){
+                    map.put(5,map.get(5)-3);
+                     map.put(20,map.get(20)+1);
+                    }
+                else
+                    return false;
+            }
+        }
+
+        return true;
+    }
+}
