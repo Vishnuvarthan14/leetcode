ccc03c25af06cd850fbdc492ee67342d4b940482
Vishnuvarthan14 <139092681+Vishnuvarthan14@users.noreply.github.com>
2025-09-04 19:49:20 +0530

    Time: 10 ms (78.84%), Space: 48.2 MB (90.53%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -3,16 +3,14 @@ class Solution {
         Arrays.sort(g);
         Arrays.sort(s);
 
-        int count=0;
         int ind=0;
         for(int i:s)
         {
             if(ind<g.length&&g[ind]<=i)
             {
-                count++;
                 ind++;
             }
         }
-        return count;
+        return ind;
     }
 }
