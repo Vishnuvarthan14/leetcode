27ec0ed4a9c560cd92787eeb4bf7bd47418ff0e0
Vishnuvarthan14 <139092681+Vishnuvarthan14@users.noreply.github.com>
2025-11-17 11:30:12 +0530

    Time: 2 ms (93.67%), Space: 47.6 MB (21.84%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,25 +1,17 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        
-        int freq[]=new int[nums.length+1];
-        int arr[]={-1,-1};
-        for(int i:nums) freq[i]++;
-
-        for(int i=1;i<nums.length+1;i++)
+        int freq[] = new int[nums.length+1];
+        int arr[] = new int[2];
+        for(int i :nums)
+        {
+            freq[i]++;
+        }
+        for(int i=1;i<=nums.length;i++)
         {
             if(freq[i]==2)
-                {
-                    arr[0]=i;
-                    break;
-                }
-        }
-        for(int i=1;i<nums.length+1;i++)
-        {
-            if(freq[i]==0)
-                {
-                    arr[1]=i;
-                    break;
-                }
+                arr[0]=i;
+            else if(freq[i]==0)
+                arr[1]=i;
         }
         return arr;
     }
