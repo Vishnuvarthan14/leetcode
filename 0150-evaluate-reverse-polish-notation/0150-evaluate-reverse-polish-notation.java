65da59ed0b49f4c25efbe51ea887c050210b9456
Vishnuvarthan14 <139092681+Vishnuvarthan14@users.noreply.github.com>
2025-11-21 18:52:04 +0530

    Time: 6 ms (76.79%), Space: 45.4 MB (19.1%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,49 +1,33 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        
         Stack <Integer> stack = new Stack<>();
-
-        for(int i=0;i<tokens.length;i++)
+        for(String s:tokens)
         {
-            String s=tokens[i];
-            if(!(s.equals("+")||s.equals("-")||s.equals("*")||s.equals("/")))
+            if(s.equals("+"))
             {
-                stack.push(Integer.parseInt(s));
+                int temp=stack.pop();
+                stack.push(temp+stack.pop());
+            }
+            else if(s.equals("*"))
+            {
+                int temp=stack.pop();
+                stack.push(temp*stack.pop());
+            }
+            else if(s.equals("-"))
+            {
+                int temp=stack.pop();
+                stack.push(stack.pop()-temp);
+            }
+            else if(s.equals("/"))
+            {
+                int temp=stack.pop();
+                stack.push(stack.pop()/temp);
             }
             else
-            {
-                if(s.equals("+"))
-                {
-                    int a=stack.pop();
-                    int b=stack.pop();
-                    int temp =b+a;
-                    stack.push(temp);
-                }
-                else if(s.equals("-"))
-                {
-                  int a=stack.pop();
-                    int b=stack.pop();
-                    int temp =b-a;
-                    stack.push(temp);
-                }
-                else if(s.equals("*"))
-                {
-                   int a=stack.pop();
-                    int b=stack.pop();
-                    int temp =b*a;
-                    stack.push(temp);
-                }
-                else if(s.equals("/"))
-                {
-                    int a=stack.pop();
-                    int b=stack.pop();
-                    int temp =b/a;
-                    stack.push(temp);
-                }
-            }
-            
-        } 
-        
-        return stack.peek();
+                stack.push(Integer.parseInt(s));
+            //System.out.println(stack);
+        }
+       
+        return stack.pop();
     }
 }
