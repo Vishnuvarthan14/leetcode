009ebde863118d6c1ca3241e11e7b44249c3a106
Vishnuvarthan14 <139092681+Vishnuvarthan14@users.noreply.github.com>
2026-01-24 11:32:41 +0530

    Time: 1771 ms (5.05%), Space: 0B (100%) - LeetHub

---
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select p.product_id,p.product_name from Product as p 
+join Sales as s on p.product_id=s.product_id
+group by p.product_id
+having sum(sale_date<"2019-01-01" or sale_date>"2019-03-31")=0;
